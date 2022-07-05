1f67f06f7697a733cc665bc9f11c30490a57522f
Alvarojj <aejimenez19@gmail.com>
2022-07-05 16:00:59 -0500

    actualizacion

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,28 +1,40 @@
 const menu = document.getElementById("menu")
-const navegation = document.getElementById("nav")
 const btn = document.getElementById("btn")
+const navegation = document.getElementById("nav")
 const btn1 = document.getElementById("btn1")
 const btn2 = document.getElementById("btn2")
 const btn3 = document.getElementById("btn3")
 let ancho = document.documentElement.clientWidth;
 
-menu.addEventListener('click', function(){
-    navegation.classList.toggle("active");
-});
-if(ancho <= 500){
-    btn.addEventListener('click', function(){
-        navegation.classList.toggle("active");
-    });
-}
 
-btn1.addEventListener('click', function(){
+
+menu.addEventListener('click', function () {
     navegation.classList.toggle("active");
 });
 
-btn2.addEventListener('click', function(){
-    navegation.classList.toggle("active");
+btn.addEventListener('click', function () {
+    navegation.classList.remove("active");
 });
 
-btn3.addEventListener('click', function(){
-    navegation.classList.toggle("active");
+btn1.addEventListener('click', function () {
+    navegation.classList.remove("active");
 });
+
+btn2.addEventListener('click', function () {
+    navegation.classList.remove("active");
+});
+
+btn3.addEventListener('click', function () {
+    navegation.classList.remove("active");
+});
+
+var typed = new Typed('.type', {
+    strings: ['<i class="titulo">BIENDENIDOS <br></i> <i class="segundotitulo">A mi portafolio.</i> '],
+    stringsElement: '#typed-strings',
+    typeSpeed:30, 
+    backSpeed: 10,
+    startDelay: 700,
+    loop: true,
+    loopCount: 5,
+    showCursor: false
+  });
