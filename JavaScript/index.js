2347a1964902a28dfa9631ad6887717460fa2e12
Alvarojj <aejimenez19@gmail.com>
2022-07-19 01:07:25 -0500

    avances en el footer

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -4,7 +4,6 @@ const navegation = document.getElementById("nav")
 const btn1 = document.getElementById("btn1")
 const btn2 = document.getElementById("btn2")
 const btn3 = document.getElementById("btn3")
-let ancho = document.documentElement.clientWidth;
 
 
 
@@ -38,3 +37,83 @@ var typed = new Typed('.type', {
     loopCount: 1,
     showCursor: false
   });
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+const objetoProyect = document.getElementById('objetoproyect')
+const btnProyect = document.getElementById('btnsProyect')
+
+
+objetoProyect.addEventListener('mouseover', function(){
+    btnProyect.classList.toggle("btnProyect");
+});
+
+objetoProyect.addEventListener('mouseout', function(){
+    btnProyect.classList.toggle("btnProyect");
+});
+
+//*************************************************************************************** */
+const objetoProyect2 = document.getElementById('objetoproyect2')
+const btnProyect2 = document.getElementById('btnsProyect2')
+
+
+objetoProyect2.addEventListener('mouseover', function(){
+    btnProyect2.classList.toggle("btnProyect");
+});
+
+objetoProyect2.addEventListener('mouseout', function(){
+    btnProyect2.classList.toggle("btnProyect");
+});
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+const objetoProyect3 = document.getElementById('objetoproyect3')
+const btnProyect3 = document.getElementById('btnsProyect3')
+
+
+objetoProyect3.addEventListener('mouseover', function(){
+    btnProyect3.classList.toggle("btnProyect");
+});
+
+objetoProyect3.addEventListener('mouseout', function(){
+    btnProyect3.classList.toggle("btnProyect");
+});
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// const objetoProyect4 = document.getElementById('objetoproyect4')
+// const btnProyect4 = document.getElementById('btnsProyect4')
+
+
+// objetoProyect4.addEventListener('mouseover', function(){
+//     btnProyect4.classList.toggle("btnProyect");
+// });
+
+// objetoProyect4.addEventListener('mouseout', function(){
+//     btnProyect4.classList.toggle("btnProyect");
+// });
+// //***************************************************************************************+ */
+
+// const objetoProyect5 = document.getElementById('objetoproyect5')
+// const btnProyect5 = document.getElementById('btnsProyect5')
+
+
+// objetoProyect5.addEventListener('mouseover', function(){
+//     btnProyect5.classList.toggle("btnProyect");
+// });
+
+// objetoProyect5.addEventListener('mouseout', function(){
+//     btnProyect5.classList.toggle("btnProyect");
+// });
+
+// //**************************************************************************************** */
+
+// const objetoProyect6 = document.getElementById('objetoproyect6')
+// const btnProyect6 = document.getElementById('btnsProyect6')
+
+
+// objetoProyect6.addEventListener('mouseover', function(){
+//     btnProyect6.classList.toggle("btnProyect");
+// });
+
+// objetoProyect6.addEventListener('mouseout', function(){
+//     btnProyect6.classList.toggle("btnProyect");
+// });
